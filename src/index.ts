a14ac953b9c456f7c0396577f2e47f6e80484ce5
wasiliev96 <wasiliev.cloud@gmail.com>
2021-01-18 15:44:13 +0200

    add basic auth through firebase

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express from "express";
-
+import {checkAuth} from "./auth";
+import cors from 'cors';
 const app = express();
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 const port = 8080; // default port to listen
 
+app.use('/', checkAuth)
 // define a route handler for the default home page
 app.get("/", (req: express.Request, res: express.Response) => {
     res.status(200).json({'message': 'Hello'})
 });
-
 // start the Express server
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
